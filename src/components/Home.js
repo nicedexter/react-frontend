60f41c6d7a3f6226a22be70ad10e7a3fded7d526
Manuel Spuhler <manuel.spuhler@gmail.com>
2018-05-29 00:17:26 +0200

    CSS grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,18 @@
 
 import React from 'react'
 import VariableContainer from './VariableContainer'
-import { Jumbotron } from 'react-bootstrap' // flowlint-line untyped-import:off
+import './Home.css'
 
 export default () => (
   <div>
-    <h2> Home </h2>
-    <Jumbotron>
-      <h1>Hello MIP</h1>
-    </Jumbotron>
-    <VariableContainer />
+    <h2>Epidemiological Exploration</h2>
+    <div className="wrapper">
+      <div className="box a">
+        <VariableContainer />
+      </div>
+      <div className="box b">B</div>
+      <div className="box c">C</div>
+      <div className="box d">D</div>
+    </div>
   </div>
 )
