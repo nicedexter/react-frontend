d169a244ac95a618c207d10226210bb61d38800f
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-07-03 10:54:35 +0200

    Apollo HOC

---
+++ src/graphql/withApollo.js
@@ -0,0 +1,48 @@
+// @flow
+
+import React from 'react'
+// FIXME: replace with Apollo Boost
+import { ApolloClient } from 'apollo-client' // flowlint-line untyped-import:off
+import { ApolloProvider } from 'react-apollo' // flowlint-line untyped-import:off
+import { InMemoryCache } from 'apollo-cache-inmemory' // flowlint-line untyped-import:off
+import { withClientState } from 'apollo-link-state' // flowlint-line untyped-import:off
+import { HttpLink } from 'apollo-link-http' // flowlint-line untyped-import:off
+import { ApolloLink } from 'apollo-link' // flowlint-line untyped-import:off
+
+import { resolvers, defaults, schema } from './'
+
+const cache = new InMemoryCache({
+  dataIdFromObject: object => {
+    switch (object.__typename) {
+      case 'Experiment':
+        return object.uuid
+      default:
+        return object.uuid || object.code
+    }
+  },
+})
+
+const stateLink = withClientState({
+  cache,
+  defaults,
+  resolvers,
+  schema,
+})
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    stateLink,
+    new HttpLink({
+      uri: 'http://localhost:3000/graphql/',
+    }),
+  ]),
+  cache,
+})
+
+const withApollo = BaseComponent => (
+  <ApolloProvider client={client}>
+    <BaseComponent />
+  </ApolloProvider>
+)
+
+export default withApollo
--- src/index.js
+++ src/index.js
@@ -3,51 +3,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-
-import { ApolloClient } from 'apollo-client' // flowlint-line untyped-import:off
-import { ApolloProvider } from 'react-apollo' // flowlint-line untyped-import:off
-import { InMemoryCache } from 'apollo-cache-inmemory' // flowlint-line untyped-import:off
-import { withClientState } from 'apollo-link-state' // flowlint-line untyped-import:off
-import { HttpLink } from 'apollo-link-http' // flowlint-line untyped-import:off
-import { ApolloLink } from 'apollo-link' // flowlint-line untyped-import:off
-import { resolvers, defaults, schema } from './graphql'
+import { compose } from 'ramda'
 
 import { App } from './components'
+import withAppolo from './graphql/withApollo'
 import registerServiceWorker from './registerServiceWorker'
 
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
 
-const cache = new InMemoryCache()
-const stateLink = withClientState({
-    cache,
-    defaults,
-    resolvers,
-    schema,
-})
-const client = new ApolloClient({
-    link: ApolloLink.from([
-        stateLink,
-        new HttpLink({
-            uri: 'http://localhost:3000/graphql/',
-        }),
-    ]),
-    cache,
-})
-
 const root = document.getElementById('root')
 if (root == null) {
   throw new Error('no root element')
 }
 
-render(
-  <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
-  </ApolloProvider>,
-  root
+const MyApp = props => (
+  <Router {...props}>
+    <App />
+  </Router>
 )
+
+render(withAppolo(MyApp), root)
 
 registerServiceWorker()
